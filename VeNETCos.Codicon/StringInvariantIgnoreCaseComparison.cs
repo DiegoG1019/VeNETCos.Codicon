9916ee2175115111bdc4b5ee9cb6ca8ac178eb4c
warlock-DiegoG <dagarciam1014@gmail.com>
2023-03-10 22:32:58 -0400

    update

---
+++ VeNETCos.Codicon/StringInvariantIgnoreCaseComparison.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace VeNETCos.Codicon;
+
+public class StringInvariantIgnoreCaseComparison : IEqualityComparer<string>
+{
+    public bool Equals(string? x, string? y)
+        => x.Equals(y, StringComparison.InvariantCultureIgnoreCase);
+
+    public int GetHashCode([DisallowNull] string obj)
+        => obj.ToLowerInvariant().GetHashCode();
+
+    private StringInvariantIgnoreCaseComparison() { }
+
+    public static StringInvariantIgnoreCaseComparison Instance { get; } = new();
+}
