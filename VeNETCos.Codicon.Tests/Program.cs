16997c80bbef0a0d28f835a183eb9479577453ae
warlock-DiegoG <dagarciam1014@gmail.com>
2023-03-11 00:21:55 -0400

    Updated tests

--- VeNETCos.Codicon.Tests/Program.cs
+++ VeNETCos.Codicon.Tests/Program.cs
@@ -4,34 +4,30 @@ using VeNETCos.Codicon.Database.Models;
 
 namespace VeNETCos.Codicon.Tests;
 
-
 internal class Program
 {
     static void Main(string[] args)
     {
         using var services = AppServices.GetServices<AppDbContext>().Get(out var context);
 
-        //context.AppBoxes.Add(new AppBox(Guid.NewGuid(), "Mi caja", "Mi cajita linda", 0));
-        //context.BoxedApps.Add(new BoxedApp(Guid.NewGuid(), "\"C:\\Users\\Personal\\Desktop\\Archivo.txt\""));
-        //context.SaveChanges();
+        context.AppBoxes.Add(new AppBox(Guid.NewGuid(), "Mi caja", "Mi cajita linda", 0));
+        context.BoxedApps.Add(new BoxedApp(Guid.NewGuid(), Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Archivo.txt")));
+        context.SaveChanges();
 
         Console.WriteLine(context.AppBoxes.Count());
 
-        //AppBox box = context.AppBoxes.Where(box => box.Id == Guid.Parse("39101818-9604-4d8f-8c42-7def77cdfd85")).First();
-        //box.Apps.Add(context.BoxedApps.FirstOrDefault());
+        AppBox box = context.AppBoxes.First();
+        BoxedApp app = context.BoxedApps.First();
 
-        BoxedApp app = context.BoxedApps.FirstOrDefault();
-        app.Boxes.Add(context.AppBoxes.FirstOrDefault());
+        box.Apps.Add(app);
+        app.Boxes.Add(box);
 
         context.SaveChanges();
-
 
         context.AppBoxes.ForEachAsync(box =>
         {
             Console.WriteLine(box.Id);
             Console.WriteLine(box.Apps.Count());
         });
-
-
     }
 }
