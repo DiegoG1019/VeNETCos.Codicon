39fba0e55672d2da47aca1e066641990aa01fe29
warlock-DiegoG <dagarciam1014@gmail.com>
2023-03-12 16:38:05 -0400

    updated

--- VeNETCos.Codicon/UI/Controls/FileLinkItemView.xaml.cs
+++ VeNETCos.Codicon/UI/Controls/FileLinkItemView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using VeNETCos.Codicon.UI.ViewModels;
 
 namespace VeNETCos.Codicon.UI.Controls;
 /// <summary>
@@ -19,8 +21,27 @@ namespace VeNETCos.Codicon.UI.Controls;
 /// </summary>
 public partial class FileLinkItemView : UserControl
 {
+    public FileLinkViewModel DataModel => (FileLinkViewModel)DataContext;
+    private readonly ILogger Log;
+
     public FileLinkItemView()
     {
         InitializeComponent();
+        Log = LoggerStore.GetLogger(this);
+    }
+
+    private void Rectangle_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        Log.Information("Trying to start a process for FileLink {file}", DataModel);
+        try
+        {
+            Process.Start(DataModel.Path);
+        }
+        catch(Exception exc)
+        {
+            Log.Warning("Could not start a process for FileLink {file}", DataModel);
+            return;
+        }
+        Log.Information("Succesfully started a process for FileLink {file}", DataModel);
     }
 }
